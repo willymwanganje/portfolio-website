1550989f49bfb38d1c0eeb56055c87f9633fd661
willymwanganje <willymwanganje@gmail.com>
2026-05-26 16:31:42 +0300

    q

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Global CSS
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
